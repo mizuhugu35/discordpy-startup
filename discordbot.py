ce1bd26c7cfe347d830bd10ff47869d2a6e15ad4
mizuhugu35 <62385334+mizuhugu35@users.noreply.github.com>
2020-03-19 22:06:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='!ap03')
+bot = commands.Bot(command_prefix='!ap03 ')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
