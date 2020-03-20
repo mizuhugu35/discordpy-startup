5f6f61357b7de51e21d248c9283b651c40933da6
mizuhugu35 <62385334+mizuhugu35@users.noreply.github.com>
2020-03-21 01:55:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='!ap03 ')
+bot = commands.Bot(command_prefix='!ap ')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -19,16 +19,31 @@ async def hello(ctx):
 
 @bot.command()
 async def helps(ctx):
-    await ctx.send('APEXキルレ0.3の会botだよ〜\n> コマンドリスト\n> `!ap03 hello` : 挨拶をするよ〜\n> `!ap03 help`  : 今表示しているやつだよ\n> `!ap03 info`  : 案内を表示するよ〜\n')
+    await ctx.send('APEXキルレ0.3の会botだよ〜\n> コマンドリスト\n> `!ap hello` : 挨拶をするよ〜\n> `!ap help`  : 今表示しているやつだよ\n> `!ap info`  : 案内を表示するよ〜\n')
 
 @bot.command()
 async def info(ctx):
-    rep = f'ようこそ!APEXキルレサーバーへ！\n\n> **システム**カテゴリの**プラットフォーム選択**よりプラットフォームが選択できるよ！\n> **全般**カテゴリより**id交換**のチャンネルに:oringin_id:ORIGIN IDまたは:ps_id:PS IDをかくとid交換できるかも！？！？'
+    rep = f'ようこそ!APEXキルレ0.3サーバーへ！\n\n> \n> **システム**カテゴリの**プラットフォーム選択**よりプラットフォームが選択できるよ！\n> \n> **全般**カテゴリより**id交換**のチャンネルに :origin_id: *ORIGIN ID*または :ps_id: *PD ID*をかくとid交換できるかも！？！？'
     await ctx.send(rep)
-    
+   
+@bot.event
+async def on_message(message):
+   if message.content.startswith('!ap add '):
+        args = message.content.split()
+        unm  = message.author.mentions
+        uid  = args[2]
+        CHANNEL_ID = 680449983425675280
+        channel = bot.get_channel(CHANNEL_ID)  
+        rep = f'{unm} {uid}'
+        await channel.send(rep)
+        
+   elif bot.user in message.mentions:
+        reply = f'{message.author.mention} なにかお困りでしたら`!ap helps`とタイプしてみてね！'
+        await message.channel.send(reply) # 返信メッセージを送信
+
 @bot.event
 async def on_member_join(member):
-    rep = f'{member.mention} ようこそ!APEXキルレサーバーへ！\n\n> \n> **システム**カテゴリの**プラットフォーム選択**よりプラットフォームが選択できるよ！\n> \n> **全般**カテゴリより**id交換**のチャンネルに*oringin*または*ps*をかくとid交換できるかも！？！？'
+    rep = f'{member.mention} ようこそ!APEXキルレ0.3サーバーへ！\n\n> \n> **システム**カテゴリの**プラットフォーム選択**よりプラットフォームが選択できるよ！\n> \n> **全般**カテゴリより**id交換**のチャンネルに :origin_id: *ORIGIN ID*または :ps_id: *PD ID*をかくとid交換できるかも！？！？'
     CHANNEL_ID = 680045070438629380
     channel = bot.get_channel(CHANNEL_ID)  
     await channel.send(rep)
